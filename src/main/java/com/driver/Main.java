263c4d544703fe72994b380ead13f23e8268de59
Pramod D K <pramoddk8590@gmail.com>
2024-08-09 15:41:19 +0530

    third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,13 +7,3 @@ public class Main {
         System.out.println(Ans);
     }
 }
-class A{
-     String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-     String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
