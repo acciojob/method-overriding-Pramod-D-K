b80b6c22229b26a63265f49710b60020c5ab7042
Pramod D K <pramoddk8590@gmail.com>
2024-08-09 15:27:53 +0530

    Bottom:

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
+public class Main {
+    public static void main(String[] args) {
 
+        B object =new B();
+        String Ans =object.meth();
+        System.out.println(Ans);
+    }
+}
 class A{
     public String meth(){
         return "Invoking method from class A";
@@ -9,14 +16,4 @@ class B extends A{
     public String meth(){
         return "Method is overridden in Extendend class B";
     }
-}
-public class Main {
-    public static void main(String[] args) {
-
-        B object =new B();
-        String Ans =object.meth();
-        System.out.println(Ans);
-
-    }
-  
 }
