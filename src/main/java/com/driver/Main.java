b56133e12ce4635da89d14dbc059a5208c9f9436
Pramod D K <pramoddk8590@gmail.com>
2024-08-09 17:54:25 +0530

    7th commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 public class Main {
     public static void main(String[] args) {
-        B obj = new B();
-        String string = obj.meth(); // This will output: "Method is overridden in Extended class B"
+        A obj = new B();
+        String string = obj.meth();
     }
 }
