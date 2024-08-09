f098a924122532482c34947a6c357bcb5b08c545
Pramod D K <pramoddk8590@gmail.com>
2024-08-09 16:33:03 +0530

    changes in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,6 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth()); // This will output: "Method is overridden in Extended class B"
+        String string = obj.meth(); // This will output: "Method is overridden in Extended class B"
     }
 }
