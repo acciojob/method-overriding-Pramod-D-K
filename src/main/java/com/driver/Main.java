a6b77a4a7cd01eb93c126abca7475ffd821ae3aa
Pramod D K <pramoddk8590@gmail.com>
2024-08-11 15:15:11 +0530

    A class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         B b1 =new B();
         System.out.println(b1.meth());
-        B b2 = new B();
+        A b2 = new B();
         System.out.println(b2.meth());
     }
 }
