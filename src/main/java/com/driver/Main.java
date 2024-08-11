41563c528337dc1ad0828584be645046b4f85c74
Pramod D K <pramoddk8590@gmail.com>
2024-08-11 11:32:07 +0530

    A and B inside Main class with out public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,12 @@
 package com.driver;
 
 public class Main {
-    public static class A{
+     static class A{
         public String meth(){
             return "Invoking method from class A";
         }
     }
-    public static class B extends A{
+     static class B extends A{
         public String meth(){
             return "Method is overridden in Extended class B";
         }
