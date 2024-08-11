358c1defe0b6892f100e2af6f3c997c60e570239
Pramod D K <pramoddk8590@gmail.com>
2024-08-11 15:40:11 +0530

    20 commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,7 +13,7 @@ public class Main {
     }
     public static void main(String[] args) {
         B b1 =new B();
-        B b2 = new B();
+       // B b2 = new B();
         System.out.println(b1.meth());
 
     }
