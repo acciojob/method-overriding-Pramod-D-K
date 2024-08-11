cc901d6588514ee2be6b4685f04ccffc6cc58fe0
Pramod D K <pramoddk8590@gmail.com>
2024-08-11 15:19:19 +0530

    nothing changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,8 +13,8 @@ public class Main {
     }
     public static void main(String[] args) {
         B b1 =new B();
+        B b2 = new B();
         System.out.println(b1.meth());
-        A b2 = new B();
         System.out.println(b2.meth());
     }
 }
