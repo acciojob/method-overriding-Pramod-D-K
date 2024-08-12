835bb3308ee5aff21402dc3609e7004acb03e19c
Pramod D K <pramoddk8590@gmail.com>
2024-08-12 12:39:22 +0530

    deleted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,6 @@ public class Main {
     }
 
     public static class B extends A {
-        @Override
         public String meth() {
             return "Method is overridden in Extended class B";
         }
