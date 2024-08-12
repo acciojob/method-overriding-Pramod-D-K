0fda6bf7d6cb73543d7e5194b4c1807067e90481
Pramod D K <pramoddk8590@gmail.com>
2024-08-12 11:51:11 +0530

    override added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,24 @@
 package com.driver;
 
 public class Main {
-     public static class A{
-        public static String meth(){
+    public static class A {
+        public String meth() {
             return "Invoking method from class A";
         }
     }
-     public static class B extends A{
 
-        public static String meth(){
+    public static class B extends A {
+        @Override
+        public String meth() {
             return "Method is overridden in Extended class B";
         }
     }
+
     public static void main(String[] args) {
-        B b1 =new B();
+        B b1 = new B();
         System.out.println(b1.meth());
-        B b2 = new B();
+
+        A b2 = new B();
         System.out.println(b2.meth());
-
-
     }
 }
